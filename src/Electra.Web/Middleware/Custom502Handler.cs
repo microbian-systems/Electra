0381aed7366be5139a6fe970bd2f431aa4362c00
bbqchickenrobot <bbqchickenrobot@lunaswap.io>
2024-05-11 12:13:32 -0500

    custom 502 handler

---
+++ src/Electra.Web/Middleware/Custom502Handler.cs
@@ -0,0 +1,22 @@
+﻿namespace Electra.Common.Web.Middleware;
+
+public class Custom502Handler(RequestDelegate next)
+{
+    private readonly RequestDelegate _next = next ?? throw new ArgumentNullException(nameof(next));
+
+    public async Task Invoke(HttpContext context)
+    {
+        await _next(context);
+
+        if (context.Response.StatusCode == StatusCodes.Status502BadGateway)
+        {
+            // Replace the response with a custom error page
+            context.Response.Clear();
+            context.Response.StatusCode = StatusCodes.Status502BadGateway;
+            context.Response.ContentType = "text/html";
+
+            // todo - add custom designed html page to incude
+            await context.Response.WriteAsync("<html><body><h1>Whoops! 502 Bad Gateway</h1><p>we are working on resolving this asap...</p></body></html>");
+        }
+    }
+}
