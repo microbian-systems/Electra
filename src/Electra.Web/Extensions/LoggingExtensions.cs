9672d50bfccbcb9261a550b8b8bf8acec8edeabe
bbqchickenrobot <bbqchickenrobot@lunaswap.io>
2024-05-23 20:01:40 -0500

    massive refactor

--- src/Electra.Web/Extensions/LoggingExtensions.cs
+++ src/Electra.Web/Extensions/LoggingExtensions.cs
@@ -8,9 +8,41 @@ public static class LoggingExtensions
 {
     public static WebApplicationBuilder AddDefaultLogging(this WebApplicationBuilder builder)
     {
-        builder.AddSerilogLogging();
+        builder.Host.UseSerilog((context, services, logConfig) =>
+        {
+            logConfig
+                .ReadFrom.Configuration(context.Configuration)
+                .Enrich.WithEnvironmentName()
+                .Enrich.WithEnvironmentUserName()
+                .Enrich.FromLogContext();
+        });
+
+        AddDefaultLogging(builder.Services, builder.Configuration);
 
         return builder;
+    }
+
+    public static IServiceCollection AddDefaultLogging(this IServiceCollection services, IConfiguration config)
+    {
+        var loggerConfig = new LoggerConfiguration()
+            .ReadFrom.Configuration(config)
+            .Enrich.WithEnvironmentName()
+            .Enrich.WithEnvironmentUserName()
+            .Enrich.FromLogContext();
+        var logger = loggerConfig.CreateLogger();
+        services.AddLogging(c =>
+        {
+            c.ClearProviders();
+            c.AddConsole();
+            //if (!config.Environment.IsProduction())
+                c.AddDebug();
+
+            c.AddSerilog(logger, dispose: true);
+            var log = logger.GetLogger();
+            log.LogInformation("Logging configured");
+        });
+
+        return services;
     }
 
     public static ILogger GetReloadableLogger(this IServiceCollection builder, IConfiguration config)
