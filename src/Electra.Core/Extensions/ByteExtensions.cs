b4dcd5e18073edc3a812da947a414f19ec45333c
bbqchickenrobot <bbqchickenrobot@lunaswap.io>
2024-06-02 15:25:38 -0500

    auth updates

---
+++ src/Electra.Core/Extensions/ByteExtensions.cs
@@ -0,0 +1,7 @@
+﻿namespace Electra.Core.Extensions;
+
+public static class ByteExtensions
+{
+    public static string ToBase64(this byte[] data) => Convert.ToBase64String(data);
+    public static byte[] FromBase64(this string data) => Convert.FromBase64String(data);
+}
