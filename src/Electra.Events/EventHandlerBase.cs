12692495705aa099d52b8a015ce804a6cdba18e6
bbqchickenrobot <bbqchickenrobot@gmail.com>
2025-05-12 16:42:50 -0500

    Electra Events

---
+++ src/Electra.Events/EventHandlerBase.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace Electra.Events;
+
+public interface IEventHandlerBase { }
+
+public abstract class EventHandlerBase(ILogger<EventHandlerBase> log) : IEventHandlerBase
+{
+    /// <summary>
+    /// Cancelation token support for event handlers
+    /// </summary>
+    /// <param name="timeout">the timeout in minutes</param>
+    /// <returns><see cref="CancellationToken"/></returns>
+    protected CancellationToken GetToken(int timeout = 10) 
+        => new CancellationTokenSource(TimeSpan.FromMinutes(timeout)).Token;
+};
