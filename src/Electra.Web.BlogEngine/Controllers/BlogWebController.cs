f25774b86071210f6c758bc5755737338870a00e
bbqchickenrobot <bbqchickenrobot@gmail.com>
2025-12-28 16:30:16 -0600

    misc

---
+++ src/Electra.Web.BlogEngine/Controllers/BlogWebController.cs
@@ -0,0 +1,9 @@
+using Electra.Web.Core.Controllers;
+using Microsoft.Extensions.Logging;
+
+namespace Electra.Web.BlogEngine.Controllers;
+
+public class BlogWebController(ILogger<ElectraWebBaseController> log) : ElectraWebBaseController(log)
+{
+    
+}
