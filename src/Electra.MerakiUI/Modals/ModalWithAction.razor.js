e9af977d5390175a5aca8fff8628a1b1053de0fd
bbqchickenrobot <bbqchickenrobot@gmail.com>
2025-12-30 05:18:40 -0600

    feat(merakiui): Convert Modal components

---
+++ src/Electra.MerakiUI/Modals/ModalWithAction.razor.js
@@ -0,0 +1,6 @@
+export class ModalWithAction {
+    static init(element) {
+        // AlpineJS initializes automatically via x-data
+    }
+}
+//# sourceMappingURL=ModalWithAction.razor.js.map
---
+++ src/Electra.MerakiUI/Modals/SimpleModal.razor.js
@@ -0,0 +1,6 @@
+export class SimpleModal {
+    static init(element) {
+        // AlpineJS initializes automatically via x-data
+    }
+}
+//# sourceMappingURL=SimpleModal.razor.js.map
