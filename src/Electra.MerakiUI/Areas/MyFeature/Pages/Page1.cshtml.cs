d4faaa11611979bcc8f101c79e0316946f12ec29
bbqchickenrobot <bbqchickenrobot@gmail.com>
2025-12-29 22:18:53 -0600

    Merakiui

---
+++ src/Electra.MerakiUI/Areas/MyFeature/Pages/Page1.cshtml.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Electra.MerakiUI.MyFeature.Pages
+{
+    public class Page1Model : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
---
+++ src/Electra.MerakiUI/Component1.razor.cs
@@ -0,0 +1,5 @@
+namespace Electra.MerakiUI;
+
+public partial class Component1
+{
+}
