f15a50217612ce95eb9249d443b3757d3d664a74
bbqchickenrobot <bbqchickenrobot@gmail.com>
2025-12-29 23:37:13 -0600

    feat(ui): Implement Input components based on MerakiUI

---
+++ src/Electra.MerakiUI/Inputs/EmailInput.razor.ts
@@ -0,0 +1,2 @@
+/* Scoped TypeScript for EmailInput */
+export {};
---
+++ src/Electra.MerakiUI/Inputs/PasswordInput.razor.cs
@@ -0,0 +1,30 @@
+using Microsoft.AspNetCore.Components;
+
+namespace Electra.MerakiUI.Inputs;
+
+public partial class PasswordInput : MerakiComponentBase
+{
+    [Parameter]
+    public string? Placeholder { get; set; } = "Password";
+
+    [Parameter]
+    public string? Value { get; set; }
+
+    [Parameter]
+    public EventCallback<string?> ValueChanged { get; set; }
+
+    protected bool ShowPassword { get; set; }
+
+    protected string InputType => ShowPassword ? "text" : "password";
+
+    protected async Task OnInput(ChangeEventArgs e)
+    {
+        Value = e.Value?.ToString();
+        await ValueChanged.InvokeAsync(Value);
+    }
+
+    protected void ToggleShowPassword()
+    {
+        ShowPassword = !ShowPassword;
+    }
+}
